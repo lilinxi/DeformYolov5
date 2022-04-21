3c730d42c94cae79892f264626c56ad04fd58ef7
limengfan.lmf <limengfan.lmf@bytedance.com>
2022-04-21 18:16:22 +0800

    commit at 2022-04-21 18:16:22:

--- models/common.py
+++ models/common.py
@@ -42,6 +42,7 @@ class Conv(nn.Module):
     def __init__(self, c1, c2, k=1, s=1, p=None, g=1, act=True):  # ch_in, ch_out, kernel, stride, padding, groups
         super().__init__()
         self.conv = nn.Conv2d(c1, c2, k, s, autopad(k, p), groups=g, bias=False)
+        # self.conv = models.deform_conv_utils.DeformConvV1_TorchVision(c1, c2, k, s, autopad(k, p), groups=g, bias=False)
         self.bn = nn.BatchNorm2d(c2)
         self.act = nn.SiLU() if act is True else (act if isinstance(act, nn.Module) else nn.Identity())
 
