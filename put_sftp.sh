aba3f985dacdafe9f8bb2061d262d4c0c15a7f54
limengfan.lmf <limengfan.lmf@bytedance.com>
2022-05-05 10:19:48 +0800

    commit at 2022-05-05 10:19:48:

--- put_sftp.sh
+++ put_sftp.sh
@@ -13,12 +13,10 @@ cd ..
 #rm -rf ./DeformYolov5Sftp
 #git clone git@github.com:lilinxi/DeformYolov5.git DeformYolov5Sftp
 
-#sftp lab << EOF
-#put -r ./DeformYolov5Sftp /home/mm
-#put ./yolov5x.pt /home/mm/DeformYolov5Sftp
-#EOF
-
-sftp lab724 << EOF
-put -r ./DeformYolov5Sftp /home/lmf
-#put -r ./DeformYolov5/yolov5x.pt /home/lmf/DeformYolov5Sftp
+sftp lab << EOF
+put -r ./DeformYolov5Sftp /home/mm
 EOF
+
+#sftp lab724 << EOF
+#put -r ./DeformYolov5Sftp /home/lmf
+#EOF
