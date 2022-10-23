9676a5b9e1a8dbedeea09d5789474e1968b761de
lilinxi <376181868@qq.com>
2022-10-23 20:58:01 +0800

    commit at 2022-10-23 20:58:01:

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,7 @@
 #python -m pip install -r requirements.txt
 conda activate Yolov5
 python server.py
+#nohup python server.py &
+
+sudo ln -s /home/lmf/anaconda3/envs/Yolov5/bin/python /usr/bin/python
+sudo python server.py
